89073811475b3d7839f203f461bb8863999979a4
Farzana Yesmin <yesminfarzana.cse@gmail.com>
2023-08-24 17:40:55 +0600

    ProductDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Product from './components/Product'
 import TopBar from './components/TopBar'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
     <TopBar/>
+    <Product/>
       
     </>
   )
